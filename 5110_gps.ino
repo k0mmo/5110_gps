7eedbc4bdb88fd8d353f616d292959ebf3b9866d
k0mmo <49208988+k0mmo@users.noreply.github.com>
2019-11-06 00:30:50 -0500

    Add files via upload

--- 5110_gps.ino
+++ 5110_gps.ino
@@ -12,79 +12,60 @@
  *  
  *  Gps pin layout:
  */
-
 #include <Adafruit_GPS.h>
-#include <Nokia_LCD.h>
+#include <SoftwareSerial.h>
 
-Nokia_LCD lcd(7 /* CLK */, 6 /* DIN */, 5 /* DC */, 4 /* CE */, 3 /* RST */); // pin layout from lcd to uno
+// change numbers to match wireing
+SoftwareSerial mySerial(0, 1);
 
-// Connect to the GPS on the hardware I2C port
-Adafruit_GPS GPS(&Wire);
+Adafruit_GPS GPS(&mySerial);
 
-// Set to 'true' to debug and listen to the raw GPS sentences
-#define GPSECHO false
-
-uint32_t timer = millis();
-
+// Set GPSECHO to 'false' to turn off echoing the GPS data, set to true for debugging and raw sentences
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  true
 
 void setup()
 {
-  while (!Serial);  // waits until Serial is ready
 
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // connect at 115200 
   Serial.begin(115200);
-  lcd.begin(); // starts lcd when powered on
-  lcd.setContrast(50); // recomended value between 40 to 60
-  lcd.setCursor(0,0); 
-  lcd.print("  Gps starting:");
-  lcd.setCursor(0,1);
-  lcd.print("________________");
-  lcd.setCursor(0,3);
-  lcd.print("Waiting for fix:");
-  Serial.println("Adafruit Gps to arduino uno r3 with nokia 5110 lcd");
+  delay(5000);
+  Serial.println("Adafruit GPS library basic test!");
 
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(0x10);  // The I2C address to use is 0x10
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.begin(9600);
 
-  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  
+  // checks to see if antenna is connected or not
   GPS.sendCommand(PGCMD_ANTENNA);
 
-  delay(1000);
-
-  // Ask for firmware version
-  GPS.println(PMTK_Q_RELEASE);
 }
 
-void loop() // run over and over again
-{
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-    if (c) Serial.print(c);
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
-  }
-  // resets timer if millis() or timer wraps around
-  if (timer > millis()) timer = millis();
+uint32_t timer = millis();
 
-  // refehes ever 2 seconds
+void loop()                     // run over and over again
+{
+  char c = GPS.read();
+  if ((c) && (GPSECHO))
+    Serial.write(c);
+
+  
+  if (GPS.newNMEAreceived()) {
+    
+    (GPS.parse(GPS.lastNMEA())); 
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
   if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
+
     Serial.print("\nTime: ");
     if (GPS.hour < 10) { Serial.print('0'); }
     Serial.print(GPS.hour, DEC); Serial.print(':');
@@ -109,19 +90,11 @@ void loop() // run over and over again
       Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
       Serial.print(", ");
       Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+
       Serial.print("Speed (knots): "); Serial.println(GPS.speed);
       Serial.print("Angle: "); Serial.println(GPS.angle);
       Serial.print("Altitude: "); Serial.println(GPS.altitude);
       Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-      lcd.clear(false);
-      lcd.setCursor(0,0);
-      lcd.print("  Gps found");
-      lcd.setCursor(0,1);
-      lcd.print("Lat: ");
-      lcd.print(GPS.latitude, 4); lcd.print(GPS.lat);
-      lcd.setCursor(0,2);
-      lcd.print("Lng: ");
-      lcd.print(GPS.longitude, 4); lcd.println(GPS.lon);
     }
   }
 }
